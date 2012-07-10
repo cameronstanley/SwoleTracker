171e7708fe647712b5ca5c0c5bb1dc01fdfe5d94
Cameron Stanley <ooprab@gmail.com>
2012-07-09 21:18:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwoleTracker::Application.config.secret_token = '4d0b4b317ede503f3bf8c69c282675ceff941082686dceaddde523e4238b4c464d26b12adc6daf3a65c65ec167584b59a661ff81b9722c9bce81f56534d969b5'
